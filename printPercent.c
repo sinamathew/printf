24c464733abc6fe1ad9275c5df4ce02542ce9a71
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-20 00:15:58 -0700

    Create % Handler

---
+++ printPercent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_percent - Print a percent character.
+ * Return: Number of characters printed
+ */
+
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
